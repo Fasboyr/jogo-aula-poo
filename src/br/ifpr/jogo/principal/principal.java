ce80e82afec173e2c32e76d6060fd877812e2598
Your Name <you@example.com>
2023-05-23 22:33:03 -0300

    Criação da janela principal

---
+++ src/br/ifpr/jogo/principal/principal.java
@@ -0,0 +1,19 @@
+package br.ifpr.jogo.principal;
+
+import javax.swing.JFrame;
+
+public class principal extends JFrame {
+    public principal(){
+        setVisible(true);
+        setSize(500, 500);
+        setTitle("Meu Jogo");
+        setLocationRelativeTo(null);
+
+
+    }
+    public static void main(String[] args) {
+        principal principal = new principal();
+        
+    }
+    
+}
